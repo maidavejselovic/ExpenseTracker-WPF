0653abe75e5fb18a2316867077565988616e827e
Maja <maidavejselovic@gmail.com>
2026-03-04 21:45:55 +0100

    first commit

---
+++ Data/ExpenseDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ExpenseTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Data
+{
+    public class ExpenseDbContext : DbContext
+    {
+        public ExpenseDbContext() : base("MySqlConnection")
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<ExpenseDbContext>());
+        }
+
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
---
+++ Models/Expense.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime ExpenseDate { get; set; }
+
+        public bool IsPaid { get; set; }
+    }
+}
