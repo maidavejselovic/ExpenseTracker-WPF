e50b47bb98d7ca736877f2b8e44b2ead169a4a6f
Maja <maidavejselovic@gmail.com>
2026-03-05 01:07:35 +0100

    changes

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ExpenseTracker.Commands
+{
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,37 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using ExpenseTracker.Data;
+﻿using ExpenseTracker.Data;
 using ExpenseTracker.Models;
-using System.Collections.ObjectModel;
 using ExpenseTracker.Windows;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
 
 namespace ExpenseTracker
 {
     public partial class MainWindow : Window
     {
-       
+        private readonly ExpenseDbContext db;
+
+        public ObservableCollection<Expense> Expenses { get; set; }
+
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+            db = new ExpenseDbContext();
 
+            // Učitavanje rashoda iz baze
+            Expenses = new ObservableCollection<Expense>(db.Expenses.ToList());
+
+            DataContext = this;
         }
+
         private void Add_Click(object sender, RoutedEventArgs e)
         {
             AddWindow addWindow = new AddWindow();
-            addWindow.ShowDialog();  
+            addWindow.ShowDialog();
+
+            RefreshExpenses();
         }
 
+        private void RefreshExpenses()
+        {
+            Expenses.Clear();
+
+            foreach (var expense in db.Expenses.ToList())
+            {
+                Expenses.Add(expense);
+            }
+        }
     }
 }
